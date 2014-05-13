700807bd94c58c32ef180107467bc47e9e9c532a
yosssi <yoshida.keiji.84@gmail.com>
2014-05-13 17:38:03 +0900

    Add a feature of serving an index file

--- options.go
+++ options.go
@@ -4,6 +4,8 @@ package staticbin
 type Options struct {
 	// SkipLogging will disable [Static] log messages when a static file is served.
 	SkipLogging bool
+	// IndexFile defines which file to serve as index if it exists.
+	IndexFile string
 }
 
 // retrieveOptions retrieves an options from the array of options.
@@ -12,6 +14,11 @@ func retrieveOptions(options []Options) Options {
 
 	if len(options) > 0 {
 		opt = options[0]
+	}
+
+	// Set the default value to opt.IndexFile.
+	if opt.IndexFile == "" {
+		opt.IndexFile = defaultIndexFile
 	}
 
 	return opt
